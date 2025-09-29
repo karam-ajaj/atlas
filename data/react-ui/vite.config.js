3a9f0080cb3883f6be4da96ebb6ec2ae55272efc
alsotoes <alsotoes@gmail.com>
2025-09-28 22:59:49 -0600

    chore: Remove rollup-plugin-visualizer
    Removes the bundle visualizer package and its configuration, as it was a temporary diagnostic tool.

--- data/react-ui/vite.config.js
+++ data/react-ui/vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      filename: './dist/stats.html',
-      open: false,
-    }),
-  ],
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
